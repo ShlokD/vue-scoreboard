953671a544b33cc200f396f267af9525e722fdfd
ShlokD <shlokpdesai@gmail.com>
2018-01-06 12:28:25 -0800

    Init app

---
+++ src/components/Scoreboard.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="scoreboard">
+    <player 
+      v-for="player in players"
+      :key=player.name
+      :playerName=player.name
+    >
+    </player>
+    </div>
+</template>
+
+<script>
+import player from './Player';
+
+export default {
+  name: 'hello',
+  data() {
+    return {
+      players: this.$store.state
+    };
+  },
+  components: {
+    player
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+
+.scoreboard {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  
+  min-width: 480px;
+  padding: 24px;
+  
+}
+</style>
